c0ad2bad4c3a2390484fbb02df9645ebd580e808
diazf <diazf@acm.org>
2024-11-12 12:54:24 -0500

    initial import

---
+++ README.md
@@ -0,0 +1,27 @@
+# notation.py
+
+for latex authors interested in maintaining notation in a json file that can be used to generate definitions files and notation tables.
+
+## structure of a notations json
+
+a json dictionary consisting of a single entry "notation" that is a list of notation definitions.  a notation definition consists of the following mandatory fields,
+   * name: the notation name; should be unique to the project
+   * definition: escaped latex defining the symbol (what would go in the definition section of newcommand).
+   * description: escaped latex describing the symbol; this will be printed in the table
+and the following optional fields,
+   * tabname: symbol name to be printed in the table (default: name)
+   * describe: boolean indicating if the symbol should be included in the notation table (default: true)
+   * define: boolean indicating if the symbol should be defined as a new command (default: true)
+   * args: number of arguments that the command will have (default: 0)
+the top level dictionary can include an optional "caption" field to contain the notation table caption.
+
+## usage
+
+to generate notation definitions,
+```
+./notation.py -I example.json
+```
+to generate a notation table,
+```
+./notation.py -I example.json -t 
+```
---
+++ notation.py
@@ -0,0 +1,55 @@
+#!/opt/homebrew/bin/python3
+import argparse
+import json
+
+def parse_json(file_path):
+    """
+    Recursively parses a JSON file, handling nested inclusions.
+
+    Args:
+        file_path: Path to the JSON file.
+
+    Returns:
+        A list of notations.
+    """
+
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    
+    return data.get('notation', []), data.get('caption', None)
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='create notation.')
+    parser.add_argument('-I', dest='input', help='json file')
+    parser.add_argument('-t', dest='table', action='store_true')  
+
+    args = parser.parse_args()
+    notations,caption = parse_json(args.input)
+    
+    if args.table:
+        print("\\begin{table}")
+        if caption is not None:
+            print("\\caption{%s}"%caption)
+        print("\\begin{tabular}{ll}")
+    lastempty=True
+    for notation in notations:
+        if args.table:
+            if len(notation) == 0 and lastempty is False:
+                print("\\\\")
+                lastempty = True
+            elif ("describe" not in notation) or (notation["describe"]):
+                name = notation["tabname"] if "tabname" in notation else f"\\{notation["name"]}"
+                print("$%s$\t&\t%s\t\\\\"%(name,notation["description"]))
+                lastempty = False
+        else:
+            if len(notation) > 0 and ("define" not in notation or notation["define"] is True):
+                print("\\newcommand{\\%s}[%d]{%s}"%(notation["name"],notation["args"] if "args" in notation else 0,notation["definition"]))
+
+    if args.table:
+        print("\\end{tabular}")
+        print("\\end{table}")
+
+if __name__ == '__main__':
+    main()
